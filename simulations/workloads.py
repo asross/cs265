e2b13214b1b0c58019379464ef0f0cdbefd93ad2
Andrew Ross <andrew@vermonster.com>
2017-03-08 17:53:42 -0500

    add more workloads and simulation notebook

--- simulations/workloads.py
+++ simulations/workloads.py
@@ -1,13 +1,27 @@
 import numpy as np
 import scipy.stats
 
+def distr(dist):
+  return '{}{}'.format(dist.dist.name, dist.args)
+
+class cacheprop(object):
+  def __init__(self, getter): self.getter = getter
+  def __get__(self, actual_self, _):
+    value = self.getter(actual_self)
+    actual_self.__dict__[self.getter.__name__] = value
+    return value
+
 class RoundRobinWorkload():
   def __init__(self, n_queries=25000, k_classes=2500):
     self.n = n_queries
     self.k = k_classes
 
-  def generate(self):
+  @cacheprop
+  def queries(self):
     return np.array([q % self.k for q in range(self.n)])
+
+  def __str__(self):
+    return 'RoundRobinWorkload'
 
 class ZipfWorkload():
   def __init__(self, n_queries=25000, zipf_param=1.1):
@@ -15,8 +29,12 @@ class ZipfWorkload():
     self.n = n_queries
     self.z = zipf_param
 
-  def generate(self):
+  @cacheprop
+  def queries(self):
     return np.random.zipf(self.z, self.n)
+
+  def __str__(self):
+    return 'ZipfWorkload({})'.format(self.z)
 
 class MultinomialWorkload():
   def __init__(self, n_queries=25000, k_classes=2500, dist=scipy.stats.uniform()):
@@ -24,39 +42,57 @@ class MultinomialWorkload():
     self.k = k_classes
     self.dist = dist
 
-  def generate(self):
-    probs = self.dist.rvs(size=self.k)
-    probs /= probs.sum()
-    return np.random.choice(self.k, size=self.n, p=probs)
+  @cacheprop
+  def queries(self):
+    self.probs = self.dist.rvs(size=self.k)
+    self.probs /= self.probs.sum()
+    return np.random.choice(self.k, size=self.n, p=self.probs)
+
+  def __str__(self):
+    return 'MultinomialWorkload({})'.format(distr(self.dist))
+
+class UniformWorkload(MultinomialWorkload):
+  def __init__(self, n_queries=25000, k_classes=2500):
+    self.n = n_queries
+    self.k = k_classes
+    self.dist = scipy.stats.uniform()
+
+  def __str__(self):
+    return 'UniformWorkload'
 
 class TimeDecayBetaMultWorkload():
   def __init__(self, n_queries=25000,
-      new_discoveries=scipy.stats.poisson(3),
-      new_popularity=scipy.stats.beta(2,2),
-      new_decay_rate=scipy.stats.beta(100,1),
-      new_time_inter=scipy.stats.expon(0.25)):
+      discoveries=scipy.stats.poisson(3),
+      popularity=scipy.stats.beta(2,2),
+      decay_rate=scipy.stats.beta(100,1),
+      time_inter=scipy.stats.expon(0.25)):
     self.n = n_queries
-    self.new_discoveries = new_discoveries
-    self.new_popularity = new_popularity
-    self.new_decay_rate = new_decay_rate
-    self.new_time_inter = new_time_inter
+    self.discoveries = discoveries
+    self.popularity = popularity
+    self.decay_rate = decay_rate
+    self.time_inter = time_inter
 
   def sample(self, pops):
     p = np.array(pops)
     p /= p.sum()
     return np.random.choice(len(p), p=p, size=10)
 
-  def generate(self):
+  def __str__(self):
+    return 'TimeDecayBetaMultWorkload(\nnew={},pop={},\ndecay={},dt={})'.format(
+        *[distr(d) for d in [self.discoveries,self.popularity,self.decay_rate,self.time_inter]])
+
+  @cacheprop
+  def queries(self):
     queries = []
     populs = []
     decays = []
     while len(queries) < self.n:
-      for i in range(self.new_discoveries.rvs()):
-        populs.append(self.new_popularity.rvs())
-        decays.append(self.new_decay_rate.rvs())
+      for i in range(self.discoveries.rvs()):
+        populs.append(self.popularity.rvs())
+        decays.append(self.decay_rate.rvs())
       if len(populs) > 0:
         for el in self.sample(populs):
           queries.append(el)
-        dt = self.new_time_inter.rvs()
+        dt = self.time_inter.rvs()
         populs = [populs[i] * decays[i]**dt for i in range(len(populs))]
     return np.array(queries)
